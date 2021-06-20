a787c01518d7021dc1c06d9ce614f6860a1225db
freesky <freesky_zh@hotmail.com>
2021-06-20 09:53:48 +0800

    change update to set for Redis

--- src/main/java/com/freesky/redis/RedisTester.java
+++ src/main/java/com/freesky/redis/RedisTester.java
@@ -30,7 +30,7 @@ public class RedisTester {
         System.out.println("连接成功");
         //查看服务是否运行
         System.out.println("服务正在运行: "+ jedis.ping());
-        jedis.append("key2", "Max");
+        jedis.set("key2", "Max");
         System.out.println("key1: " + jedis.get("key1"));
         System.out.println("key2: " + jedis.get("key2"));
 
