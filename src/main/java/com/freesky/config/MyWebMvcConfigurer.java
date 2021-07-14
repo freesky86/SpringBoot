e699bf1bc7c70ce3cec3c833a6277f220268bfb5
maxzhang <freesky_zh@hotmail.com>
2021-07-14 18:33:13 +0800

    Add some new classes.

--- src/main/java/com/freesky/config/MyWebMvcConfigurer.java
+++ src/main/java/com/freesky/config/MyWebMvcConfigurer.java
@@ -10,14 +10,16 @@ import com.freesky.controller.interceptor.TwoInterceptor;
 @Configuration
 public class MyWebMvcConfigurer implements WebMvcConfigurer {
 
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        /**
-         * 拦截器按照顺序执行
-         */
-        registry.addInterceptor(new OneInterceptor()).addPathPatterns("/one/**");
-        registry.addInterceptor(new TwoInterceptor()).addPathPatterns("/two/**").addPathPatterns("/one/**");
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		/**
+		 * 拦截器按照顺序执行
+		 */
+//		registry.addInterceptor(new OneInterceptor()).addPathPatterns("/*/**"); // 拦截所有请求
+		
+		registry.addInterceptor(new OneInterceptor()).addPathPatterns("/one/**");
+		registry.addInterceptor(new TwoInterceptor()).addPathPatterns("/two/**").addPathPatterns("/one/**");
 
-    }
+	}
 
 }
--- src/main/java/com/freesky/springboot/DemoApplication.java
+++ src/main/java/com/freesky/springboot/DemoApplication.java
@@ -3,15 +3,18 @@ package com.freesky.springboot;
 import org.springframework.boot.Banner;
 import org.springframework.boot.ResourceBanner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.io.ClassPathResource;
 
+@SpringBootApplication
 public class DemoApplication {
 	public static void main(String[] args) {
 //        SpringApplication.run(DemoConfiguration.class, args);
 		
 		// extend startup behaviour
-		SpringApplication bootstrap = new SpringApplication(DemoConfiguration.class);
-		bootstrap.setBanner(new ResourceBanner(new ClassPathResource("banner.txt")));
+//		SpringApplication bootstrap = new SpringApplication(DemoConfiguration.class);
+		SpringApplication bootstrap = new SpringApplication(DemoApplication.class);
+		bootstrap.setBanner(new ResourceBanner(new ClassPathResource("test.txt")));
 		bootstrap.setBannerMode(Banner.Mode.CONSOLE);
         // 其他定制设置...
         bootstrap.run(args);
