1ab68b169d2ad8d4e1f7d0ebef327435fa249a3f
freesky <freesky_zh@hotmail.com>
2022-02-06 17:49:04 +0800

    format code

--- src/main/java/com/freesky/jms/Receiver.java
+++ src/main/java/com/freesky/jms/Receiver.java
@@ -4,14 +4,13 @@
 package com.freesky.jms;
 
 import org.springframework.jms.annotation.JmsListener;
-import org.springframework.stereotype.Component;
 
 /**
  * Receiver is a message-driven POJO.
  * 
  * @author freesky
  */
-@Component
+// @Component
 public class Receiver {
 
     @JmsListener(destination = "mailbox", containerFactory = "myFactory")
